9d38b070733c14e8cdba64e5156001acceec8290
malak73 <malak73a@gmail.com>
2020-03-23 08:46:16 +0200

    fixing errors

--- app/src/main/java/y2019/aoc/malak/malakaoc2019/MainActivity.java
+++ app/src/main/java/y2019/aoc/malak/malakaoc2019/MainActivity.java
@@ -3,6 +3,7 @@ package y2019.aoc.malak.malakaoc2019;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,13 +14,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "FIREBASE";
     Button signIn, logIn;
     MediaPlayer mMediaPlayer;
     private VideoView videoBG;
     int mCurrentVideoPosition;
     EditText editTextPassword, editTextEmail;
+
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -27,13 +37,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         videoBG = (VideoView) findViewById(R.id.videoView);
         signIn = findViewById(R.id.signIn);
         signIn.setOnClickListener(this);
         logIn = findViewById(R.id.logIn);
         logIn.setOnClickListener(this);
+        editTextEmail=findViewById(R.id.editTextEmail);
+        editTextEmail.setOnClickListener(this);
+        editTextPassword=findViewById(R.id.editTextPassword);
+        editTextPassword.setOnClickListener(this);
+
 
 
         Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.welcomevideo);
@@ -89,16 +105,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v == logIn) {
             if (editTextPassword.getText().toString().equals("") || editTextEmail.getText().toString().equals("")) {
                 Toast.makeText(this, " Empty Password or Email", Toast.LENGTH_LONG).show();
-                Intent i = new Intent(this, LogIn.class);
+
 
             } else {
+                login(editTextEmail.getText().toString(), editTextPassword.getText().toString());
 
-
-                Intent i = new Intent(this, AccountTypeActivity.class);
-                i.putExtra("email", editTextEmail.getText().toString());
-                i.putExtra("password", editTextPassword.getText().toString());
-                startActivity(i);
 
             }
     }
-}}
+
+}
+    public void login(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(MainActivity.this, LogIn.class);
+                            startActivity(i);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
